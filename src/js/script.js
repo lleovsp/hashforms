6298f5f99844a659bdf8b668927f1b19dcb1b214
Leonardo Vinícius <leonardo.frpsouza@gmail.com>
2025-05-02 17:11:08 -0300

    commit inicial

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const campoCPF = document.querySelector("#cpf");
+    console.log(campoCPF);
+    const regex = /^\d+$/;
+    campoCPF.addEventListener("input", (e) => {
+        if(!regex.test(campoCPF.value.slice(-1))){
+            console.log("teste funcionando");
+            campoCPF.value = campoCPF.value.slice(0, -1);
+        }
+    });
+
+    // tratamento do formulário
+    document.querySelector("form").addEventListener("submit", event => { 
+        let dadosEndereco = [];
+        let dadosPessoais = [];
+        let controller = true;
+        let dataBoxes = document.querySelectorAll("input");
+        dataBoxes.forEach(element => {
+            if(element.value === "" || element.value === " ")
+                {
+                    controller = false;
+                }
+            });
+            if(controller === false){
+                event.preventDefault();
+                alert("Preencha todos os campos para continuar");
+            
+                
+            } 
+            else{
+                console.log("Todos os dados estão corretos");
+                dataBoxes.forEach(element => {
+                    if (element.parentElement.parentElement.id === "dados-pessoais"){
+                        dadosPessoais.push(element.value);
+                        console.log(dadosPessoais);
+                    }
+                    if (element.parentElement.parentElement.id === "endereco"){
+                        dadosEndereco.push(element.value);
+                        console.log(dadosEndereco);
+                    }
+                    localStorage.setItem("Dados Pessoais", dadosPessoais);
+                    localStorage.setItem("Endereço", dadosEndereco);
+                });
+            }
+        });
+    });
