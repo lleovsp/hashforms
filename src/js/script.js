7206561f99fa06b3c4d91b771e03bbb1d1706d38
Leonardo Vinícius <leonardo.frpsouza@gmail.com>
2025-05-02 17:53:13 -0300

    git

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    
     const campoCPF = document.querySelector("#cpf");
-    console.log(campoCPF);
     const regex = /^\d+$/;
     campoCPF.addEventListener("input", (e) => {
         if(!regex.test(campoCPF.value.slice(-1))){
-            console.log("teste funcionando");
             campoCPF.value = campoCPF.value.slice(0, -1);
+        }
+        
+        if(campoCPF.value === 3){
+            console.log("if do ponto")
+            campoCPF.value += "."
         }
     });
 
     // tratamento do formulário
     document.querySelector("form").addEventListener("submit", event => { 
+        
         let dadosEndereco = [];
         let dadosPessoais = [];
         let controller = true;
         let dataBoxes = document.querySelectorAll("input");
+
         dataBoxes.forEach(element => {
             if(element.value === "" || element.value === " ")
                 {
@@ -29,6 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 
             } 
             else{
+                
                 console.log("Todos os dados estão corretos");
                 dataBoxes.forEach(element => {
                     if (element.parentElement.parentElement.id === "dados-pessoais"){
