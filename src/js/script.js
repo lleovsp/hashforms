a473c12c2fafcb8fb39e293720c9be9cfb591a65
Leonardo Vinícius <leonardo.frpsouza@gmail.com>
2025-05-05 09:27:41 -0300

    melhoria de performance

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    
     const campoCPF = document.querySelector("#cpf");
     const regex = /^\d+$/;
-    campoCPF.addEventListener("input", (e) => {
-        if(!regex.test(campoCPF.value.slice(-1))){
+    campoCPF.addEventListener("input", () => {
+        if (!regex.test(campoCPF.value.slice(-1))) {
             campoCPF.value = campoCPF.value.slice(0, -1);
         }
-        
-        if(campoCPF.value === 3){
+
+        if (campoCPF.value === 3) {
             console.log("if do ponto")
             campoCPF.value += "."
         }
     });
 
     // tratamento do formulário
-    document.querySelector("form").addEventListener("submit", event => { 
-        
+    document.querySelector("form").addEventListener("submit", event => {
+        event.preventDefault();
         let dadosEndereco = [];
         let dadosPessoais = [];
         let controller = true;
         let dataBoxes = document.querySelectorAll("input");
-
-        dataBoxes.forEach(element => {
-            if(element.value === "" || element.value === " ")
-                {
-                    controller = false;
+        for (let i = 0; i < dataBoxes.length; i++) {
+            let element = dataBoxes[i];
+            if (element.value.trim() === "") {
+                alert("Preencha todos os campos para continuar");
+                event.preventDefault();
+                controller = false;
+                break;
+            }
+        }
+        if (controller === true) {
+            console.log("Todos os dados estão corretos");
+            dataBoxes.forEach(element => {
+                if (element.parentElement.parentElement.id === "dados-pessoais") {
+                    dadosPessoais.push(element.value);
+                    console.log(dadosPessoais);
+                }
+                if (element.parentElement.parentElement.id === "endereco") {
+                    dadosEndereco.push(element.value);
+                    console.log(dadosEndereco);
                 }
             });
-            if(controller === false){
-                event.preventDefault();
-                alert("Preencha todos os campos para continuar");
-            
-                
-            } 
-            else{
-                
-                console.log("Todos os dados estão corretos");
-                dataBoxes.forEach(element => {
-                    if (element.parentElement.parentElement.id === "dados-pessoais"){
-                        dadosPessoais.push(element.value);
-                        console.log(dadosPessoais);
-                    }
-                    if (element.parentElement.parentElement.id === "endereco"){
-                        dadosEndereco.push(element.value);
-                        console.log(dadosEndereco);
-                    }
-                    localStorage.setItem("Dados Pessoais", dadosPessoais);
-                    localStorage.setItem("Endereço", dadosEndereco);
-                });
-            }
-        });
+            localStorage.setItem("Dados Pessoais", dadosPessoais);
+            localStorage.setItem("Endereço", dadosEndereco);
+        }
     });
+});
